441208fc74fc4d79357f9e0aab172129ac9bd244
djpb <djpbadenhorst@gmail.com>
2024-01-12 21:25:34 +0200

    Small change to samba

--- containers/samba/scripts/run.sh
+++ containers/samba/scripts/run.sh
@@ -9,6 +9,6 @@ docker run -d \
        -e "UID_djpb=1000" \
        -e "SAMBA_VOLUME_CONFIG_djpb=[djpb]; path=/shares/homes/%U; valid users = djpb; guest ok = no; read only = no; browseable = yes" \
        -v /etc/avahi/services/:/external/avahi \
-       -v ~/storage/shared:/shares/homes \
+       -v ~/Workspace/storage/shared:/shares/homes \
        --restart=always \
        --name=samba ghcr.io/servercontainers/samba
