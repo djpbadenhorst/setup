17b6cd2275e8b462379d3eb7ae792e58b0537ae6
djpb <djpbadenhorst@gmail.com>
2024-03-15 16:35:05 +0200

    Fixing kodi container

--- installs/gopass.sh
+++ installs/gopass.sh
@@ -1,3 +1,5 @@
 sudo apt install -y gnupg rng-tools
 wget https://github.com/gopasspw/gopass/releases/download/v1.15.11/gopass_1.15.11_linux_amd64.deb
 sudo dpkg -i gopass_1.15.11_linux_amd64.deb
+rm gopass_1.15.11_linux_amd64.deb 
+
