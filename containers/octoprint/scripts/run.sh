bc8f7deb1993ccfaef354d8c562344c3fdd47d89
djpb <djpbadenhorst@gmail.com>
2024-03-31 22:04:48 +0200

    combined todo with deprecated

--- containers/octoprint/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-       -p 80:80 \
-       --network=host \
-       --device /dev/ttyAMA0:/dev/ttyAMA0 \
-       -v ~/octoprint:/octoprint \
-       --restart=always \
-       --name octoprint octoprint/octoprint
