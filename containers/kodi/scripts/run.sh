e1879d747e8b1f2523c7ac1e91adb4f7d702bc6c
djpb <djpbadenhorst@gmail.com>
2024-03-31 21:15:49 +0200

    Moved kodi to kasm

--- containers/kodi/scripts/run.sh
+++ containers/kodi/scripts/run.sh
@@ -1,23 +1,24 @@
-echo "Enter Password:"
-read VNC_PASSWORD
 mkdir -p /home/djpb/Workspace/storage
-docker run -d \
+docker run -d   \
        --privileged \
-       --shm-size 1g \
-       -p 10000:10000 \
-       -e VNC_PASSWD=$VNC_PASSWORD \
-       -e PORT=10000 \
-       -e VNC_PORT=5900 \
-       -e AUDIO_PORT=1699 \
-       -e WEBSOCKIFY_PORT=6900 \
-       -e SCREEN_WIDTH=1024 \
-       -e SCREEN_HEIGHT=768 \
-       -e SCREEN_DEPTH=24 \
-       -v /home/djpb/.docker/data/kodi/config:/home/ubuntu/.kodi/ \
-       --mount type=bind,src=/home/djpb/Workspace/storage/,dst=/home/ubuntu/storage/ \
+       --name=kodi \
+       -p 3000:3000 \
+       -v /dev/input:/dev/input \
+       -v /run/udev/data:/run/udev/data \
+       -v /home/djpb/.docker/data/kodi:/config/.kodi/ \
+       --mount type=bind,src=/home/djpb/Workspace/storage/,dst=/config/storage/ \
+       --device /dev/dri:/dev/dri \
        --restart=always \
-       --name=kodi kodi
+       kodi
 
-docker cp ./docker/NFAuthentication.key kodi:/home/ubuntu
-docker exec -it kodi sudo apt install kodi-inputstream-adaptive
-#PIN: 2580
+echo "docker exec -it kodi bash"
+echo ""
+echo "sudo chown abc:abc /config/.kodi/"
+echo "nano /config/.config/openbox/autostart"
+echo "> kodi"
+echo ""
+echo "nano /kclient/index.js"
+echo "> chunk.length < 40000"
+
+docker cp ./docker/NFAuthentication.key kodi:/config/
+#PIN: 3445
--- containers/qbittorrent/scripts/run.sh
+++ containers/qbittorrent/scripts/run.sh
@@ -1,6 +1,19 @@
 mkdir -p ~/Workspace/storage/torrents
-mkdir -p /home/djpb/.docker/data/qbittorrent/private
+mkdir -p /home/djpb/.docker/data/qbittorrent-private/private
+mkdir -p /home/djpb/.docker/data/qbittorrent-private/partial
 mkdir -p /home/djpb/.docker/data/qbittorrent/partial
+docker run -d \
+       --name qbittorrent-private \
+       --net=host \
+       -e PUID=1000 \
+       -e PGID=1000 \
+       -e TZ="ZA" \
+       -e WEBUI_PORTS="9001/tcp,9001/udp" \
+       -v ~/.docker/data/qbittorrent-private/config:/config \
+       --mount type=bind,src=/home/djpb/.docker/data/qbittorrent-private/private,dst=/downloads/private/ \
+       --mount type=bind,src=/home/djpb/.docker/data/qbittorrent-private/partial,dst=/downloads/partial/ \
+       --restart=always \
+       ghcr.io/hotio/qbittorrent
 docker run -d \
        --name qbittorrent \
        --net=host \
@@ -10,8 +23,8 @@ docker run -d \
        -e WEBUI_PORTS="9000/tcp,9000/udp" \
        -v ~/.docker/data/qbittorrent/config:/config \
        --mount type=bind,src=/home/djpb/Workspace/storage/torrents/,dst=/downloads/torrents/ \
-       --mount type=bind,src=/home/djpb/.docker/data/qbittorrent/private,dst=/downloads/private/ \
        --mount type=bind,src=/home/djpb/.docker/data/qbittorrent/partial,dst=/downloads/partial/ \
        --restart=always \
        ghcr.io/hotio/qbittorrent
+
 #/app/vuetorrent
