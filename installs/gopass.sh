16e2dd08100d8a72c69623698b3e3736fd66fd88
djpb <djpbadenhorst@gmail.com>
2024-04-10 22:17:30 +0200

    Added fix to gopass

--- installs/gopass.sh
+++ installs/gopass.sh
@@ -1,11 +1,11 @@
 sudo apt install -y gnupg rng-tools
 export ARCH=$(arch)
-if [ ARCH == "x86_64" ]
+if [ $ARCH == "x86_64" ]
 then
     wget https://github.com/gopasspw/gopass/releases/download/v1.15.11/gopass_1.15.11_linux_amd64.deb
     sudo dpkg -i gopass_1.15.11_linux_amd64.deb
     rm gopass_1.15.11_linux_amd64.deb 
-elif [ ARCH == "aarch64" ] 
+elif [ $ARCH == "aarch64" ] 
 then
     wget https://github.com/gopasspw/gopass/releases/download/v1.15.11/gopass_1.15.11_linux_arm64.deb
     sudo dpkg -i gopass_1.15.11_linux_arm64.deb
