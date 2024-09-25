543a88960bcff457891b3889f545d044d1b4e72d
djpbadenhorst <djpbadenhorst@gmail.com>
2024-09-25 21:11:12 +0200

    Added cura instructions

--- installs/cura.sh
+++ installs/cura.sh
@@ -1,3 +1,14 @@
-sudo apt install -y python3-pip
-pip3 install trimesh
-sudo apt install -y cura
+wget https://github.com/Ultimaker/Cura/releases/download/5.8.1/UltiMaker-Cura-5.8.1-linux-X64.AppImage
+sudo mv UltiMaker-Cura-5.8.1-linux-X64.AppImage /usr/bin/cura
+sudo chmod +x /usr/bin/cura
+
+echo "" >  /usr/share/applications/cura.desktop
+cat << EOF >  /usr/share/applications/cura.desktop
+[Desktop Entry]
+Name=Cura
+Exec=cura
+Terminal=false
+Type=Application
+StartupNotify=true
+Icon=cura
+EOF
