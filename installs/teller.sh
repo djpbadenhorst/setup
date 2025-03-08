1fd36544f02a13f64becea4adf4c966ad1d74097
djpb <djpbadenhorst@gmail.com>
2025-03-08 10:02:06 +0200

    Modified teller and rust install

--- installs/teller.sh
+++ installs/teller.sh
@@ -1,7 +1,13 @@
-git clone https://github.com/tellerops/teller
-cd teller
-git checkout tags/v1.5.6
-make build
-sudo mv teller /usr/bin/
-cd ../
-rm -rf teller
+#git clone https://github.com/tellerops/teller
+#cd teller
+#git checkout tags/v1.5.6
+#make build
+#sudo mv teller /usr/bin/
+#cd ../
+#rm -rf teller
+
+wget https://github.com/tellerops/teller/releases/download/v1.5.6/teller_1.5.6_Linux_x86_64.tar.gz
+tar -xvf ./teller_1.5.6_Linux_x86_64.tar.gz
+rm ./README.md
+rm ./LICENSE.txt
+rm teller_1.5.6_Linux_x86_64.tar.gz
