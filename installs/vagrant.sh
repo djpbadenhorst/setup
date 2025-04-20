30bd45d1b15e6c1e4e2507adb80e18fd3f007352
djpb <djpbadenhorst@gmail.com>
2025-04-20 18:39:40 +0200

    Add vagrant and gitignore

--- installs/vagrant.sh
+++ installs/vagrant.sh
@@ -3,7 +3,10 @@ unzip vagrant_2.4.3_linux_amd64.zip
 rm vagrant_2.4.3_linux_amd64.zip
 rm LICENSE.txt
 sudo mv vagrant /usr/bin/
+sudo apt install -y libfuse2
 
-sudo apt install virtualbox
-vagrant box add ubuntu/jammy64
-vagrant box add archman/linuxmint
+
+sudo apt install -y virtualbox
+#vagrant box add ubuntu/jammy64
+#vagrant box add archman/linuxmint
+vagrant box add bento/ubuntu-24.04
